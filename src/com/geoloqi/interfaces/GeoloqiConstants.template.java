67e15fc45d25bffca2a9ce17380f78ad1858c635
Aaron Parecki <aaron@parecki.com>
2011-09-24 15:36:22 -0700

    Template config file, added readme and license documents.

--- src/com/geoloqi/interfaces/GeoloqiConstants.template.java
+++ src/com/geoloqi/interfaces/GeoloqiConstants.template.java
@@ -6,11 +6,11 @@ public final class GeoloqiConstants {
 	public static final int uploadPort = 40000;
 	public static final int downloadPort = 40001;
 	public static final String URL_BASE = "https://api.geoloqi.com/1/";
-	public static final String GEOLOQI_ID = "7bcd478f26bd14b1fed18e9fe59e506f";
-	public static final String GEOLOQI_SECRET = "8bf6952f46f325311a67d5a3ed90c9a0";
+	public static final String GEOLOQI_ID = "";
+	public static final String GEOLOQI_SECRET = "";
 	public static final String PREFERENCES_FILE = "GEOLOQIHTTPCLIENT";
 	public static final String version = "1";
 
-	public static final String gameListAddress = "https://api.geoloqi.com/1/layer/nearby?application_id=R";
+	public static final String gameListAddress = "https://api.geoloqi.com/1/layer/nearby?application_id=?";
 	public static final String gameRequestAddress = "http://mapattack.org/join/";
 }
