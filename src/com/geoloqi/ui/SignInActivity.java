b850051780b30c09b652379757abb803dc696dd9
Tristan Waddington <tristan.waddington@gmail.com>
2011-10-03 00:08:30 -0700

    Implement OnClickListener instead of OnTouchListener for the submit button in the SignInActivity.

--- src/com/geoloqi/ui/SignInActivity.java
+++ src/com/geoloqi/ui/SignInActivity.java
@@ -8,9 +8,8 @@ import android.content.Context;
 import android.content.SharedPreferences.Editor;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,7 +18,7 @@ import com.geoloqi.mapattack.R;
 import com.geoloqi.interfaces.GeoloqiConstants;
 import com.geoloqi.rpc.AccountMonitor;
 
-public class SignInActivity extends Activity {
+public class SignInActivity extends Activity implements OnClickListener {
 
 	Pattern emailPattern = Pattern.compile("^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$", Pattern.CASE_INSENSITIVE);
 
@@ -27,45 +26,8 @@ public class SignInActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sign_in_activity);
-		findViewById(R.id.submit_button).setOnTouchListener(submitButtonListener);
+		findViewById(R.id.submit_button).setOnClickListener(this);
 	}
-
-	OnTouchListener submitButtonListener = new OnTouchListener() {
-
-		@Override
-		public boolean onTouch(View view, MotionEvent evt) {
-			if (evt.getAction() == MotionEvent.ACTION_UP) {
-				String initials, email;
-				{// Grep input.
-					initials = ((EditText) findViewById(R.id.initials)).getText().toString();
-					email = ((EditText) findViewById(R.id.email)).getText().toString().toUpperCase();
-				}
-				{// Test validity
-					boolean twoInitials = initials.length() == 2;
-					boolean validEmail = emailPattern.matcher(email).matches();
-					if (!twoInitials) {
-						ADB.makeToast(SignInActivity.this, "Two initials please.", Toast.LENGTH_LONG);
-						return false;
-					} else if (!validEmail) {
-						ADB.makeToast(SignInActivity.this, "Invalid e-mail", Toast.LENGTH_LONG);
-						return false;
-					}
-				}
-				new SignIn().execute();
-				{// Write to preferences.
-					Editor prefs = getSharedPreferences(GeoloqiConstants.PREFERENCES_FILE, Context.MODE_PRIVATE).edit();
-					prefs.putString("initials", initials);
-					prefs.putString("email", email).commit();
-				}
-				// Start login.
-				AccountMonitor.createUserID(SignInActivity.this);
-				// Finish.
-				setResult(RESULT_OK);
-				finish();
-			}
-			return false;
-		}
-	};
 
 	class SignIn extends AsyncTask<Void, Void, Void> {
 
@@ -85,6 +47,41 @@ public class SignInActivity extends Activity {
 		@Override
 		public void onPostExecute(Void result) {
 			progressDialog.dismiss();
+		}
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.submit_button:
+			String initials, email;
+			{// Grep input.
+				initials = ((EditText) findViewById(R.id.initials)).getText().toString();
+				email = ((EditText) findViewById(R.id.email)).getText().toString().toUpperCase();
+			}
+			{// Test validity
+				boolean twoInitials = initials.length() == 2;
+				boolean validEmail = emailPattern.matcher(email).matches();
+				if (!twoInitials) {
+					ADB.makeToast(SignInActivity.this, "Two initials please.", Toast.LENGTH_LONG);
+					return;
+				} else if (!validEmail) {
+					ADB.makeToast(SignInActivity.this, "Invalid e-mail", Toast.LENGTH_LONG);
+					return;
+				}
+			}
+			new SignIn().execute();
+			{// Write to preferences.
+				Editor prefs = getSharedPreferences(GeoloqiConstants.PREFERENCES_FILE, Context.MODE_PRIVATE).edit();
+				prefs.putString("initials", initials);
+				prefs.putString("email", email).commit();
+			}
+			// Start login.
+			AccountMonitor.createUserID(SignInActivity.this);
+			
+			// Finish.
+			setResult(RESULT_OK);
+			finish();
 		}
 	}
 
