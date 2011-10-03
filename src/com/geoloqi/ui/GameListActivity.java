2a195d525c10a3eafd6ab6d0b38f3d20962f1800
Tristan Waddington <tristan.waddington@gmail.com>
2011-10-03 00:11:05 -0700

    Major updates to the GameListActivity layout. Implement OnClickListener instead of OnTouchListener for refresh button. Don't requery for games on every orientation change. Open GeoLoqi website when the logo is clicked.

--- src/com/geoloqi/ui/GameListActivity.java
+++ src/com/geoloqi/ui/GameListActivity.java
@@ -8,16 +8,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,36 +31,58 @@ import com.geoloqi.interfaces.RPCException;
 import com.geoloqi.rpc.MapAttackClient;
 import com.geoloqi.services.GeoloqiPositioning;
 
-public class GameListActivity extends ListActivity {
+public class GameListActivity extends ListActivity implements OnClickListener {
+	public static final String TAG = "GameListActivity";
 
-	private Game selection = null;
-	Intent positioningIntent;
+	public static final String PARAM_SYNC_ON_START = "sync_on_start";
 
-	OnTouchListener refreshListener = new OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent evt) {
-			if (evt.getAction() == MotionEvent.ACTION_UP) {
-				new AsyncRequestGames().execute();
-			}
-			return view.onTouchEvent(evt);
-		}
-	};
+	/** Stub */
+	private Intent mPositioningIntent;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game_list_activity);
-		((Button) findViewById(R.id.refresh_button)).setOnTouchListener(refreshListener);
-
-		positioningIntent = new Intent(this, GeoloqiPositioning.class);
+		
+		boolean syncOnStart = true;
+		if (savedInstanceState != null) {
+			// Restore our saved instance state
+			syncOnStart = savedInstanceState.getBoolean(PARAM_SYNC_ON_START, true);
+		}
+		
+		// Find our views
+		final Button refreshButton = (Button) findViewById(R.id.refresh_button);
+		final ImageButton geoloqiButton = (ImageButton) findViewById(R.id.geoloqi);
+		
+		// Set our on click listeners
+		refreshButton.setOnClickListener(this);
+		geoloqiButton.setOnClickListener(this);
+		
+		// Reference our positioning service Intent
+		mPositioningIntent = new Intent(this, GeoloqiPositioning.class);
+		
+		if (syncOnStart) {
+			// Start our positioning service
+			stopService(mPositioningIntent);
+			startService(mPositioningIntent);
+			
+			// Search for nearby games
+			new RequestGamesListTask(this).execute();
+		}
 	}
 
 	@Override
-	public void onStart() {
-		super.onStart();
-		stopService(positioningIntent);
-		startService(positioningIntent);
-		new AsyncRequestGames().execute();
+	public void onDestroy() {
+		super.onDestroy();
+		stopService(mPositioningIntent);
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		
+		// Don't sync just because the device orientation changed
+		outState.putBoolean(PARAM_SYNC_ON_START, false);
 	}
 
 	private Location getLastKnownLocation() {
@@ -73,24 +98,24 @@ public class GameListActivity extends ListActivity {
 	}
 
 	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		stopService(positioningIntent);
-	}
-
-	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		selection = (Game) l.getItemAtPosition(position);
+		final Game selection = (Game) l.getItemAtPosition(position);
 		startActivity(new Intent(this, MapAttackActivity.class).putExtra("id", selection.id));
 	}
 
-	class AsyncRequestGames extends AsyncTask<Void, Void, List<Game>> {
+	/** Stub */
+	class RequestGamesListTask extends AsyncTask<Void, Void, List<Game>> {
+		final ProgressDialog mProgressDialog;
 
-		ProgressDialog progressDialog;
+		public RequestGamesListTask(final Context context) {
+			mProgressDialog = new ProgressDialog(context);
+			mProgressDialog.setTitle(null);
+			mProgressDialog.setMessage("Searching for nearby games...");
+		}
 
 		@Override
 		protected void onPreExecute() {
-			progressDialog = ProgressDialog.show(GameListActivity.this, "Map Attack!", "Getting nearby games.");
+			mProgressDialog.show();
 		}
 
 		@Override
@@ -100,14 +125,15 @@ public class GameListActivity extends ListActivity {
 				if (location == null) {
 					return null;
 				}
-				List<Game> games = MapAttackClient.getApplicationClient(GameListActivity.this).getGames(location.getLatitude(), location.getLongitude());
+				List<Game> games = MapAttackClient.getApplicationClient(
+						GameListActivity.this).getGames(location.getLatitude(), location.getLongitude());
 				ADB.log("Found " + games.size() + (games.size() == 1 ? " game." : " games."));
 				for (Game game : games) {
 					ADB.log(game.name + ": " + game.description);
 				}
 				return games;
 			} catch (RPCException e) {
-				ADB.makeToast(GameListActivity.this, "Game Server Unavailable", Toast.LENGTH_LONG);
+				ADB.makeToast(GameListActivity.this, "The game server is unavailable!", Toast.LENGTH_LONG);
 				return null;
 			}
 		}
@@ -115,15 +141,14 @@ public class GameListActivity extends ListActivity {
 		@Override
 		protected void onPostExecute(List<Game> games) {
 			if (games != null) {
-				setListAdapter(new GameArrayAdapter(GameListActivity.this, R.layout.game_list_element, games.toArray(new Game[games.size()])));
+				setListAdapter(new GameArrayAdapter(GameListActivity.this,
+						R.layout.game_list_element, games.toArray(new Game[games.size()])));
 			}
-			progressDialog.dismiss();
+			mProgressDialog.dismiss();
 		}
-
 	}
 
 	class GameArrayAdapter extends ArrayAdapter<Game> {
-
 		public GameArrayAdapter(Context context, int textViewResourceId, Game[] objects) {
 			super(context, textViewResourceId, objects);
 		}
@@ -133,10 +158,12 @@ public class GameListActivity extends ListActivity {
 		public void setGameList(List<Game> games) {
 			this.games = games;
 		}
-
+		
 		@Override
-		public View getView(int position, View convertView, ViewGroup group) {
-			LinearLayout element = (LinearLayout) LinearLayout.inflate(GameListActivity.this, R.layout.game_list_element, null);
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO: This seems overly complex. Refactor!
+			LinearLayout element = (LinearLayout) LinearLayout.inflate(GameListActivity.this,
+					R.layout.game_list_element, null);
 			Game game = this.getItem(position);
 			((TextView) element.findViewById(R.id.name)).setText(game.name);
 			((TextView) element.findViewById(R.id.description)).setText(game.description);
@@ -144,4 +171,19 @@ public class GameListActivity extends ListActivity {
 		}
 	}
 
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()) {
+		case R.id.refresh_button:
+			new RequestGamesListTask(this).execute();
+			return;
+		case R.id.geoloqi:
+			final Intent geoloqiIntent = new Intent(Intent.ACTION_VIEW,
+					Uri.parse("https://geoloqi.com/"));
+			geoloqiIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			geoloqiIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(geoloqiIntent);
+			return;
+		}
+	}
 }
