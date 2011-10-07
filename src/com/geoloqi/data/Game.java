e5ebf8453c6569191568e04199eaabe3ab5381d4
Tristan Waddington <tristan.waddington@gmail.com>
2011-10-06 22:50:57 -0700

    Refactor the Game container class to implement Parcelable so that it can be passed in to the savedInstanceState Bundle.

--- src/com/geoloqi/data/Game.java
+++ src/com/geoloqi/data/Game.java
@@ -3,18 +3,47 @@ package com.geoloqi.data;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Game {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-	public final String name, description, id;
+public class Game implements Parcelable {
+	public String id;
+	public String name;
+	public String description;
+
+	public static final Parcelable.Creator<Game> CREATOR = new Parcelable.Creator<Game>() {
+		public Game createFromParcel(Parcel in) {
+			return new Game(in);
+		}
+		
+		public Game[] newArray(int size) {
+			return new Game[size];
+		}
+	};
 
 	public Game(JSONObject data) {
 		try {
+			id = data.getString("layer_id");
 			name = data.getString("name");
 			description = data.getString("description");
-			id = data.getString("layer_id");
 		} catch (JSONException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
+	private Game(Parcel in) {
+		id = in.readString();
+		name = in.readString();
+		description = in.readString();
+	}
+
+	public int describeContents() {
+		return 0;
+	}
+
+	public void writeToParcel(Parcel out, int flags) {
+		out.writeString(id);
+		out.writeString(name);
+		out.writeString(description);
+	}
 }
